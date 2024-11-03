13132913956d8f907d8d98cafc06ac9ac9e356f4
jaozindograu2008 <137068450+jaozindograu2008@users.noreply.github.com>
2024-11-03 19:44:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Jogos',
+    'Quem é o criador de GD',
+    'É o RobTop'
+)
+
+criaCartao(
+    'Jogos',
+    'Quem verificou a Sakupen Circles',
+    'O Diamond'
+)
+
+criaCartao(
+    'Jogos',
+    'Quem tomou exposed por usar hack no GD',
+    'O SpaceUK'
+)
